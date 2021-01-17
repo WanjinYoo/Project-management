8164698f94af0852bb959224d93dd1220ed4d952
WanjinYoo <dksladjsla@gmail.com>
2021-01-16 16:18:29 -0800

    modify tickets table

---
+++ database/migrations/2021_01_17_001001_add_columns_to_tickets.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToTickets extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->timestamp('start_at', $precision = 0);
+            $table->string('priority_level');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->dropColumn('start_at');
+            $table->dropColumn('priority_level');
+        });
+    }
+}
