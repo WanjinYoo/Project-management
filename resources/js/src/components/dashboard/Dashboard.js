062b8ab830506370c9ba4bca240e940edb9a75ba
zhaxxdev <alex.xian@Hotmail.com>
2021-01-18 15:29:51 -0800

    added react live  clock, attempting to add statistics widget. not working

--- resources/js/src/components/dashboard/Dashboard.js
+++ resources/js/src/components/dashboard/Dashboard.js
@@ -1,10 +1,14 @@
 import React from "react";
 import LineAnnotationChart from "./LineAnnotationChart";
+import StatisticsWidget from "../hyper/components/StatisticsWidget";
+import Clock from "react-live-clock";
 
 export default function Dashboard() {
     return (
         <div>
             <h1>Dashboard</h1>
+            <Clock format={"HH:mm:ss"} ticking={true} timezone={"US/Pacific"} />
+            <StatisticsWidget />
             <LineAnnotationChart />
         </div>
     );
