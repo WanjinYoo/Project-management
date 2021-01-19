a9fd2e0de49e4bfd5fdbde4bc7259503af22d6af
zhaxxdev <alex.xian@Hotmail.com>
2021-01-19 13:56:58 -0800

    styling fixes and clock update.

--- resources/js/src/components/dashboard/Dashboard.js
+++ resources/js/src/components/dashboard/Dashboard.js
@@ -2,12 +2,22 @@ import React from "react";
 import LineAnnotationChart from "./LineAnnotationChart";
 import StatisticsWidget from "../hyper/components/StatisticsWidget";
 import Clock from "react-live-clock";
+import "../../componentsstyling/dashboard.scss";
 
 export default function Dashboard() {
     return (
         <div>
             <h1>Dashboard</h1>
-            <Clock format={"HH:mm:ss"} ticking={true} timezone={"US/Pacific"} />
+            <div id="dashboardClock">
+                <p>Today is: </p>
+                <Clock format={"dddd, MMMM Mo, YYYY"} timezone={"US/Pacific"} />
+                <p>The time is: </p>
+                <Clock
+                    format={"h:mm:ss A"}
+                    ticking={true}
+                    timezone={"US/Pacific"}
+                />
+            </div>
             <StatisticsWidget />
             <LineAnnotationChart />
         </div>
