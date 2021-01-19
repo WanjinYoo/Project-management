2646441d6e98db7957bfe22e3bd6687c32116562
Marcio Cruz <marciocruz@Marcios-MacBook-Pro.local>
2021-01-18 18:41:06 -0800

    update database migration and seeds - correct dates

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -6,15 +6,16 @@ use App\Project;
 use Faker\Generator as Faker;
 
 $factory->define(Project::class, function (Faker $faker) {
+    $create_date = $faker->dateTimeBetween($startDate = '-1 year', $endDate = 'now', $timezone = null);
     return [
         'name' => $faker->word,
         'product'=> $faker->word,
         'description' => $faker->text($maxNbChars = 200),
         'github' => $faker->unique()->userName,
-        'created_at' => $faker->dateTimeBetween($startDate = '-1 year', $endDate = 'now', $timezone = null),
-        'finished_at'=> $faker->optional($weight = 0.5)->dateTimeBetween($startDate = '-1 year', $endDate = 'now', $timezone = null),
-        'deadline' => $faker->dateTimeBetween($startDate = '-1 year', $endDate = '+1 year', $timezone = null),
-        'start_date' => $faker->dateTimeBetween($startDate = '-1 year', $endDate = '+1 year', $timezone = null),
+        'created_at' => $create_date,
+        'finished_at'=> $faker->optional($weight = 0.5)->dateTimeBetween($startDate = $create_date, $endDate = 'now', $timezone = null),
+        'deadline' => $faker->dateTimeBetween($startDate = $create_date, $endDate = '+1 year', $timezone = null),
+        'start_date' => $faker->dateTimeBetween($startDate = $create_date, $endDate = '+1 year', $timezone = null),
         'status_id' => $faker->numberBetween($min = 1, $max = 6)
     ];
 });
