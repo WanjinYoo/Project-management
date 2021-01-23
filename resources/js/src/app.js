73e60a0ddeb6a589eb0b80d1967f359ba0fb4561
zhaxxdev <alex.xian@Hotmail.com>
2021-01-22 19:59:38 -0800

    admindash

--- resources/js/src/app.js
+++ resources/js/src/app.js
@@ -1,79 +1,73 @@
 import React from "react";
 
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import Tickets from "./components/tickets/tickets";
 import Progress from "./components/progress";
 import Settings from "./components/settings";
 import Project from "./components/projects/projects";
-import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core/styles';
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
 
 import HorizontalNavbar from "./components/HorizontalNavbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import Calendar from "./components/Calendar";
-import Home from "./components/home"
+import Home from "./components/home";
 import "./app.scss";
 import projects from "./components/projects/projects";
 
 const mapStateToProps = state => {
     return {
-        logIn: state.logIn,
+        logIn: state.logIn
     };
-  }
-  const useStyles = makeStyles((theme) => ({
+};
+const useStyles = makeStyles(theme => ({
     toolbar: {
-        paddingRight: 24, // keep right padding when drawer closed
-      },
+        paddingRight: 24 // keep right padding when drawer closed
+    },
     appBarSpacer: theme.mixins.toolbar,
     content: {
         flexGrow: 1,
-        height: '100vh',
-        overflow: 'auto',
-      },
+        height: "100vh",
+        overflow: "auto"
+    },
     container: {
         paddingTop: theme.spacing(4),
-        paddingBottom: theme.spacing(4),
+        paddingBottom: theme.spacing(4)
     },
     root: {
-        display: 'flex',
-      },
-    }));
+        display: "flex"
+    }
+}));
 
 function App(props) {
     const classes = useStyles();
     return (
         <BrowserRouter>
-             {props.logIn.isLoggedIn &&
-
+            {props.logIn.isLoggedIn && (
                 <div className={classes.root}>
-
-                        <HorizontalNavbar />
+                    <HorizontalNavbar />
 
                     <main className={classes.content}>
                         <div className={classes.appBarSpacer} />
-                            <Container maxWidth="lg" className={classes.container}>
-                                <Switch>
-                                    <Route
-                                        exact={true}
-                                        path="/"
-                                        component={Dashboard}
-                                    />
-                                    <Route path="/projects" component={Project} />
-                                    <Route path="/tickets" component={Tickets} />
-                                    <Route path="/calendar" component={Calendar} />
-                                    <Route path="/progress" component={Progress} />
-                                    <Route path="/report" component={Settings} />
-                                </Switch>
-                            </Container>
+                        <Container maxWidth="lg" className={classes.container}>
+                            <Switch>
+                                <Route
+                                    exact={true}
+                                    path="/"
+                                    component={Dashboard}
+                                />
+                                <Route path="/projects" component={Project} />
+                                <Route path="/tickets" component={Tickets} />
+                                <Route path="/calendar" component={Calendar} />
+                                <Route path="/progress" component={Progress} />
+                                <Route path="/report" component={Settings} />
+                            </Switch>
+                        </Container>
                     </main>
-
-
                 </div>
-
-            }
+            )}
             {!props.logIn.isLoggedIn && <Home />}
-
         </BrowserRouter>
     );
 }
---
+++ resources/js/src/components/adminDash/adminDash.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../componentsstyling/adminDash.scss";
+
+export default function adminDash() {
+    return (
+        <div>
+            <p>Progress</p>
+        </div>
+    );
+}
