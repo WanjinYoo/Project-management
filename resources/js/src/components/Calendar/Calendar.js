282012f48c81311bdcf3e03a5c1e504f5f7057bd
zhaxxdev <alex.xian@Hotmail.com>
2021-01-26 23:24:41 -0800

    calandeer workinggit add *git add * :D:D:D:D:D

--- resources/js/src/components/Calendar/Calendar.js
+++ resources/js/src/components/Calendar/Calendar.js
@@ -1,9 +1,23 @@
 import React, { useState } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+const localizer = momentLocalizer(moment);
+import "react-big-calendar/lib/sass/styles.scss";
+export default function fCalendar() {
+    let state = {
+        events: []
+    };
 
-export default function Calendar() {
     return (
         <div>
             <h1>Your Calendar</h1>
+            <Calendar
+                localizer={localizer}
+                events={state.events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 500 }}
+            />
         </div>
     );
 }
